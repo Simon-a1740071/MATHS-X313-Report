57f0f5d7f505a2621f746a7b1f6072fe29a42710
Simon-a1740071 <a1740071@adelaide.edu.au>
2026-03-25 14:28:21 +1030

    Removing the prefix X in Year column

--- DataClean.R
+++ DataClean.R
@@ -5,7 +5,6 @@ read.csv(
   as.tibble() -> temp_df
 
 head(temp_df)
-temp_df 
 sapply(temp_df, class) #checking year columns if theyre numeric
 colnames(temp_df)
 
@@ -20,4 +19,5 @@ temp_df |>
   pivot_wider(
     names_from = "DataSeries",
     values_from = "Value"
-  ) 
+  ) |>
+  mutate(Year = stringr::str_remove(Year, "X"))
