6847f823d180491401fa3afe4e0cb31f95b50aee
Simon-a1740071 <a1740071@adelaide.edu.au>
2026-03-25 13:56:49 +1030

    Loading stringr and trying to wrangle the data here

--- DataClean.R
+++ DataClean.R
@@ -1,12 +1,21 @@
-pacman::p_load(fpp3)
+pacman::p_load(fpp3, stringr)
 
 read.csv(
   here::here("BirthsAndFertilityRatesAnnual.csv")) |>
   as.tibble() -> temp_df
 
 head(temp_df)
+temp_df 
+sapply(temp_df, class) #checking year columns if theyre numeric
+colnames(temp_df)
 
 temp_df |>
+  filter(DataSeries != cols = contains("Live Births"))
+
+
+  mutate(across(where(is.character), as.numeric))
+
+
   pivot_longer(
     cols = contains("X20"),
     names_to = "year",
