cf806356d29777e3610dc1dae56d73d9c08a8398
Simon-a1740071 <a1740071@adelaide.edu.au>
2026-03-25 15:01:57 +1030

    renaming variables

--- DataClean.R
+++ DataClean.R
@@ -4,9 +4,8 @@ read.csv(
   here::here("BirthsAndFertilityRatesAnnual.csv")) |>
   as.tibble() -> temp_df
 
-head(temp_df)
 sapply(temp_df, class) #checking year columns if theyre numeric
-colnames(temp_df)
+colnames(temp_df) #check colnames
 
 temp_df |> 
   mutate(across(where(is.character) & -any_of("DataSeries"),
@@ -20,5 +19,33 @@ temp_df |>
     names_from = "DataSeries",
     values_from = "Value"
   ) |>
-  mutate(Year = as.integer(stringr::str_remove(Year, "X"))) -> rates_df
+  mutate(Year = as.integer(stringr::str_remove(Year, "X")))|>
+  mutate(rename(
+    #newName = oldName
+    "GRR" = "Gross Reproduction Rate",
+    "CBR" = "Crude Birth Rate",
+    "RLB" = "Resident Live-Births",
+    "TFR" = "Total Fertility Rate (TFR)",
+    "TLB" = "Total Live-Births",
+    "CLB" = "Citizen Live-Births",
+    "NPR" = "Net Reproduction Rate",
+    "15-19Y" = "    15 - 19 Years",
+    "20-24Y" = "    20 - 24 Years",
+    "25-39Y" = "    25 - 29 Years",
+    "30-34Y" = "    30 - 34 Years",
+    "35-39Y" = "    35 - 39 Years",
+    "40-44Y" = "    40 - 44 Years",
+    "45-49Y" = "    45 - 49 Years"
+  )) -> rates_df
+
+colnames(rates_df) #these names are have blank spaces
+#will need to clean them up so it's easier to analyse 
+
+head(rates_df)
+
+
+
+
+rates_df |>
+  as_tsibble(index = Year)
   
