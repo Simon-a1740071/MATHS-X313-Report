c786d548b14dd8aa7d14460b0f269b4d030e6921
Simon-a1740071 <a1740071@adelaide.edu.au>
2026-03-26 14:56:28 +1030

    Changed spelling for net reprodcuction rate

--- DataClean.R
+++ DataClean.R
@@ -30,14 +30,17 @@ temp_df |>
     "TFR" = "Total Fertility Rate (TFR)",
     "TLB" = "Total Live-Births",
     "CLB" = "Citizen Live-Births",
-    "NPR" = "Net Reproduction Rate",
+    "NRR" = "Net Reproduction Rate",
     "15-19Y" = "    15 - 19 Years",
     "20-24Y" = "    20 - 24 Years",
     "25-39Y" = "    25 - 29 Years",
     "30-34Y" = "    30 - 34 Years",
     "35-39Y" = "    35 - 39 Years",
     "40-44Y" = "    40 - 44 Years",
-    "45-49Y" = "    45 - 49 Years"
+    "45-49Y" = "    45 - 49 Years",
+    "Chinese" = "    Chinese",
+    "Malays" = "    Malays",
+    "Indians" = "    Indians" 
   ) -> temp_df #updates the dataframe
 
 temp_df |> #converts to time series dateframe
